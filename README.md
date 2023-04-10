fe3e9f9731105c8564062ca4603f5a1b56784496
Seungchul Ha <seungchulha@Seungchului-MacBookPro.local>
2023-04-11 01:11:16 +0900

    edit board and comment

--- README.md
+++ README.md
@@ -31,7 +31,10 @@ target_metadata = models.Base.metadata
 alembic revision --autogenerate
 alembic upgrade head
 
-
+pip install "pydantic[email]"
+pip install "passlib[bcrypt]"
+pip install python-multipart
+pip install "python-jose[cryptography]"
 
 
 
@@ -40,3 +43,10 @@ alembic upgrade head
 npm create vite@latest frontend -- --template svelte
 npm install
 npm run dev
+
+* Svelte 라우터
+npm install svelte-spa-router
+
+npm install bootstrap
+npm install moment
+npm install qs
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,20 +1,24 @@
 <script>
-  async function hello() {
-    const res = await fetch("http://127.0.0.1:8000/hello");
-    const json = await res.json();
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
+  import Detail from "./routes/Detail.svelte"
+  import QuestionCreate from "./routes/QuestionCreate.svelte"
+  import Navigation from "./components/Navigation.svelte"
+  import UserCreate from "./routes/UserCreate.svelte"
+  import UserLogin from "./routes/UserLogin.svelte"
+  import QuestionModify from "./routes/QuestionModify.svelte"
+  import AnswerModify from "./routes/AnswerModify.svelte"
 
-    if (res.ok) {
-      return json.message;
-    } else {
-      alert("error");
-    }
+  const routes = {
+    '/': Home,
+    '/detail/:question_id': Detail,
+    '/question-create': QuestionCreate,
+    '/user-create': UserCreate,
+    '/user-login': UserLogin,
+    '/question-modify/:question_id': QuestionModify,
+    '/answer-modify/:answer_id': AnswerModify,
   }
-
-  let promise = hello();
 </script>
 
-{#await promise}
-  <p>...waiting</p>
-{:then message}
-  <h1>{message}</h1>
-{/await}
+<Navigation />
+<Router {routes}/>
